586b41d7e894bb1e2d74e7d08642b76732b7e9e9
huangqimeng <hqm_vip@163.com>
2020-01-04 23:24:29 +0800

    vue指令

---
+++ vue-02/command.js
@@ -0,0 +1,12 @@
+var vm = new Vue({
+	el: '#command',
+	data: {
+		seen: false,
+		url: 'https://cn.vuejs.org/v2/guide/computed.html',
+	},
+	methods: {
+		doSomething: function() {
+			
+		}
+	}
+})
