4dc57b89fe625c3734c6da36e2a2bb25fe45aca6
huangqimeng <hqm_vip@163.com>
2020-01-09 18:52:56 +0800

    格式化

--- vue-03/computed-watch.js
+++ vue-03/computed-watch.js
@@ -1,41 +1,41 @@
 var computeVM = new Vue({
-	el: '#compute',
-	data: {
-		msg: 'hello',
-		firstName: 'Foo',
-		lastName: 'Bar',
-	},
-	// 计算属性
-	computed: {
-		// 计算属性的 getter
-		reversedMsg: function() {
-			// this 指向 vm 实例
-			return this.msg.split('').reverse().join('')
-		},
-		
+  el: '#compute',
+  data: {
+    msg: 'hello',
+    firstName: 'Foo',
+    lastName: 'Bar',
+  },
+  // 计算属性
+  computed: {
+    // 计算属性的 getter
+    reversedMsg: function () {
+      // this 指向 vm 实例
+      return this.msg.split('').reverse().join('')
+    },
+
 		/**
 		 * 计算属性默认只有 getter，不过在需要时你也可以提供一个 setter 
 		 */
-		fullName: {
-			// getter
-			get: function() {
-				return this.firstName + ' ' + this.lastName
-			},
-			// setter
-			set: function(newVal) {
-				var names = newVal.split(' ')
-				this.firstName = names[0]
-				this.lastName = names[names.length - 1]
-			}
-		}
-	},
-	
+    fullName: {
+      // getter
+      get: function () {
+        return this.firstName + ' ' + this.lastName
+      },
+      // setter
+      set: function (newVal) {
+        var names = newVal.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[names.length - 1]
+      }
+    }
+  },
+
 	/**
 	 * 像绑定普通属性一样在模板中绑定计算属性。Vue 知道 vm.reversedMessage 依赖于 vm.message，
 	 * 因此当 vm.message 发生改变时，所有依赖 vm.reversedMessage 的绑定也会更新。
 	 * 而且最妙的是我们已经以声明的方式创建了这种依赖关系：计算属性的 getter 函数是没有副作用 (side effect) 的，这使它更易于测试和理解。
 	 */
-	
+
 	/**
 	 * 可以将同一函数定义为一个方法而不是一个计算属性。两种方式的最终结果确实是完全相同的。
 	 * 然而，不同的是 计算属性是基于它们的响应式依赖进行缓存的。只在相关响应式依赖发生改变时它们才会重新求值。
@@ -58,41 +58,41 @@ var computeVM = new Vue({
  * 并在我们得到最终结果前，设置中间状态。这些都是计算属性无法做到的。
  */
 var watchVM = new Vue({
-	el: '#watch',
-	data: {
-		question: '',
-		answer: 'I cannot give u a answer until u ask a question!',
-	},
-	watch: {
-		// 如果 `question` 发生改变，这个函数就会运行
-		question: function(newQA, oldQA) {
-			this.answer = 'Waiting for u to stop typing...'
-			this.debouncedGetAnswer()
-		}
-	},
-	created: function() {
-		// `_.debounce` 是一个通过 Lodash 限制操作频率的函数。
-		// 在这个例子中，我们希望限制访问 yesno.wtf/api 的频率
-		// AJAX 请求直到用户输入完毕才会发出。想要了解更多关于
-		// `_.debounce` 函数 (及其近亲 `_.throttle`) 的知识，
-		// 请参考：https://lodash.com/docs#debounce
-		this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
-	},
-	methods: {
-		getAnswer: function() {
-			if (this.question.indexOf('?') === -1) {
-				this.answer = 'Question usually contain a question mark. ;-'
-				return
-			}
-			this.answer = 'Thinking...'
-			var vm = this
-			axios.get('https://yesno.wtf/api')
-			.then(function(response) {
-				vm.answer = _.capitalize(response.data.answer)
-			})
-			.catch(function(error) {
-				vm.answer = 'Error! Could not reach the API.' + error
-			})
-		}
-	}
+  el: '#watch',
+  data: {
+    question: '',
+    answer: 'I cannot give u a answer until u ask a question!',
+  },
+  watch: {
+    // 如果 `question` 发生改变，这个函数就会运行
+    question: function (newQA, oldQA) {
+      this.answer = 'Waiting for u to stop typing...'
+      this.debouncedGetAnswer()
+    }
+  },
+  created: function () {
+    // `_.debounce` 是一个通过 Lodash 限制操作频率的函数。
+    // 在这个例子中，我们希望限制访问 yesno.wtf/api 的频率
+    // AJAX 请求直到用户输入完毕才会发出。想要了解更多关于
+    // `_.debounce` 函数 (及其近亲 `_.throttle`) 的知识，
+    // 请参考：https://lodash.com/docs#debounce
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+  },
+  methods: {
+    getAnswer: function () {
+      if (this.question.indexOf('?') === -1) {
+        this.answer = 'Question usually contain a question mark. ;-'
+        return
+      }
+      this.answer = 'Thinking...'
+      var vm = this
+      axios.get('https://yesno.wtf/api')
+        .then(function (response) {
+          vm.answer = _.capitalize(response.data.answer)
+        })
+        .catch(function (error) {
+          vm.answer = 'Error! Could not reach the API.' + error
+        })
+    }
+  }
 })
